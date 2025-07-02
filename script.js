337a777522a27d28e4fc3d48dd2e57b461297e3a
Vedant Verma <162734158+VedantVerma06@users.noreply.github.com>
2025-07-02 12:12:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(reg => console.log('Service Worker registered!'))
+      .catch(err => console.error('Service Worker failed: ', err));
+  });
+}
